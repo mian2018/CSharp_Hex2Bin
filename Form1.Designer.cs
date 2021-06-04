acd11379b49ee51c9aa84f77aeced8453d84772b
张 <39999734+mian2018@users.noreply.github.com>
2021-06-04 16:27:42 +0800

    1.修复历史菜单为空时，打开文件失败

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,6 +29,7 @@ namespace CSharp_Hex2Bin
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
@@ -38,7 +39,26 @@ namespace CSharp_Hex2Bin
             this.FileSize = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.StartAddr = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.EndAddr = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataViewMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.打开文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.生成binToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bin另存为ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.删除ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.打开记录ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem5 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem6 = new System.Windows.Forms.ToolStripMenuItem();
+            this.填充空段ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txbLog = new System.Windows.Forms.TextBox();
+            this.cmbLogMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.清空文本框ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openHex = new System.Windows.Forms.OpenFileDialog();
+            this.saveBin = new System.Windows.Forms.SaveFileDialog();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.dataViewMenu.SuspendLayout();
+            this.cmbLogMenu.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataGridView
@@ -70,6 +90,7 @@ namespace CSharp_Hex2Bin
             this.FileSize,
             this.StartAddr,
             this.EndAddr});
+            this.dataGridView.ContextMenuStrip = this.dataViewMenu;
             dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Window;
             dataGridViewCellStyle3.Font = new System.Drawing.Font("楷体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
@@ -78,50 +99,194 @@ namespace CSharp_Hex2Bin
             dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
             dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
             this.dataGridView.DefaultCellStyle = dataGridViewCellStyle3;
-            this.dataGridView.Location = new System.Drawing.Point(1, 28);
+            this.dataGridView.Location = new System.Drawing.Point(1, 1);
             this.dataGridView.Name = "dataGridView";
             this.dataGridView.ReadOnly = true;
             this.dataGridView.RowHeadersVisible = false;
             this.dataGridView.RowTemplate.Height = 23;
             this.dataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView.Size = new System.Drawing.Size(800, 290);
+            this.dataGridView.Size = new System.Drawing.Size(800, 317);
             this.dataGridView.TabIndex = 0;
             this.dataGridView.DragDrop += new System.Windows.Forms.DragEventHandler(this.dataGridView_DragDrop);
             this.dataGridView.DragEnter += new System.Windows.Forms.DragEventHandler(this.dataGridView_DragEnter);
             // 
             // FliePath
             // 
-            this.FliePath.FillWeight = 44F;
+            this.FliePath.FillWeight = 55F;
             this.FliePath.HeaderText = "文件路径";
             this.FliePath.Name = "FliePath";
             this.FliePath.ReadOnly = true;
             // 
             // FileSize
             // 
-            this.FileSize.FillWeight = 16F;
+            this.FileSize.FillWeight = 15F;
             this.FileSize.HeaderText = "文件大小";
             this.FileSize.Name = "FileSize";
             this.FileSize.ReadOnly = true;
             // 
             // StartAddr
             // 
-            this.StartAddr.FillWeight = 16F;
+            this.StartAddr.FillWeight = 15F;
             this.StartAddr.HeaderText = "起始地址";
             this.StartAddr.Name = "StartAddr";
             this.StartAddr.ReadOnly = true;
             // 
             // EndAddr
             // 
-            this.EndAddr.FillWeight = 16F;
+            this.EndAddr.FillWeight = 15F;
             this.EndAddr.HeaderText = "结束地址";
             this.EndAddr.Name = "EndAddr";
             this.EndAddr.ReadOnly = true;
+            // 
+            // dataViewMenu
+            // 
+            this.dataViewMenu.Font = new System.Drawing.Font("楷体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.dataViewMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.打开文件ToolStripMenuItem,
+            this.生成binToolStripMenuItem,
+            this.bin另存为ToolStripMenuItem,
+            this.删除ToolStripMenuItem,
+            this.打开记录ToolStripMenuItem,
+            this.填充空段ToolStripMenuItem});
+            this.dataViewMenu.Name = "dataViewMenu";
+            this.dataViewMenu.Size = new System.Drawing.Size(181, 158);
+            // 
+            // 打开文件ToolStripMenuItem
+            // 
+            this.打开文件ToolStripMenuItem.Image = global::CSharp_Hex2Bin.Properties.Resources.打开;
+            this.打开文件ToolStripMenuItem.Name = "打开文件ToolStripMenuItem";
+            this.打开文件ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.打开文件ToolStripMenuItem.Text = "打开hex";
+            this.打开文件ToolStripMenuItem.Click += new System.EventHandler(this.打开文件ToolStripMenuItem_Click);
+            // 
+            // 生成binToolStripMenuItem
+            // 
+            this.生成binToolStripMenuItem.Image = global::CSharp_Hex2Bin.Properties.Resources.生成;
+            this.生成binToolStripMenuItem.Name = "生成binToolStripMenuItem";
+            this.生成binToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.生成binToolStripMenuItem.Text = "生成bin";
+            this.生成binToolStripMenuItem.Click += new System.EventHandler(this.生成binToolStripMenuItem_Click);
+            // 
+            // bin另存为ToolStripMenuItem
+            // 
+            this.bin另存为ToolStripMenuItem.Image = global::CSharp_Hex2Bin.Properties.Resources.另存为;
+            this.bin另存为ToolStripMenuItem.Name = "bin另存为ToolStripMenuItem";
+            this.bin另存为ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.bin另存为ToolStripMenuItem.Text = "bin另存为";
+            this.bin另存为ToolStripMenuItem.Click += new System.EventHandler(this.bin另存为ToolStripMenuItem_Click);
+            // 
+            // 删除ToolStripMenuItem
+            // 
+            this.删除ToolStripMenuItem.Image = global::CSharp_Hex2Bin.Properties.Resources.清除;
+            this.删除ToolStripMenuItem.Name = "删除ToolStripMenuItem";
+            this.删除ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.删除ToolStripMenuItem.Text = "删除";
+            this.删除ToolStripMenuItem.Click += new System.EventHandler(this.删除ToolStripMenuItem_Click);
+            // 
+            // 打开记录ToolStripMenuItem
+            // 
+            this.打开记录ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem3,
+            this.toolStripMenuItem4,
+            this.toolStripMenuItem5,
+            this.toolStripMenuItem6});
+            this.打开记录ToolStripMenuItem.Image = global::CSharp_Hex2Bin.Properties.Resources.历史记录;
+            this.打开记录ToolStripMenuItem.Name = "打开记录ToolStripMenuItem";
+            this.打开记录ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.打开记录ToolStripMenuItem.Text = "打开记录";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem2.Text = " ";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem3.Text = " ";
+            this.toolStripMenuItem3.Click += new System.EventHandler(this.toolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem4
+            // 
+            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem4.Text = " ";
+            this.toolStripMenuItem4.Click += new System.EventHandler(this.toolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem5
+            // 
+            this.toolStripMenuItem5.Name = "toolStripMenuItem5";
+            this.toolStripMenuItem5.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem5.Text = " ";
+            this.toolStripMenuItem5.Click += new System.EventHandler(this.toolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem6
+            // 
+            this.toolStripMenuItem6.Name = "toolStripMenuItem6";
+            this.toolStripMenuItem6.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem6.Text = " ";
+            this.toolStripMenuItem6.Click += new System.EventHandler(this.toolStripMenuItem_Click);
+            // 
+            // 填充空段ToolStripMenuItem
+            // 
+            this.填充空段ToolStripMenuItem.Image = global::CSharp_Hex2Bin.Properties.Resources.填充;
+            this.填充空段ToolStripMenuItem.Name = "填充空段ToolStripMenuItem";
+            this.填充空段ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.填充空段ToolStripMenuItem.Text = "填充空段";
+            this.填充空段ToolStripMenuItem.ToolTipText = "勾选后，未指定具体值的flash地址填充0x00";
+            this.填充空段ToolStripMenuItem.Click += new System.EventHandler(this.填充空段ToolStripMenuItem_Click);
+            // 
+            // txbLog
+            // 
+            this.txbLog.ContextMenuStrip = this.cmbLogMenu;
+            this.txbLog.Location = new System.Drawing.Point(1, 324);
+            this.txbLog.Multiline = true;
+            this.txbLog.Name = "txbLog";
+            this.txbLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txbLog.Size = new System.Drawing.Size(800, 122);
+            this.txbLog.TabIndex = 1;
+            this.txbLog.WordWrap = false;
+            // 
+            // cmbLogMenu
+            // 
+            this.cmbLogMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.清空文本框ToolStripMenuItem});
+            this.cmbLogMenu.Name = "cmbLogMenu";
+            this.cmbLogMenu.Size = new System.Drawing.Size(134, 26);
+            // 
+            // 清空文本框ToolStripMenuItem
+            // 
+            this.清空文本框ToolStripMenuItem.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.清空文本框ToolStripMenuItem.Font = new System.Drawing.Font("楷体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.清空文本框ToolStripMenuItem.Image = global::CSharp_Hex2Bin.Properties.Resources.清除;
+            this.清空文本框ToolStripMenuItem.Name = "清空文本框ToolStripMenuItem";
+            this.清空文本框ToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.清空文本框ToolStripMenuItem.Text = "清空文本框";
+            this.清空文本框ToolStripMenuItem.Click += new System.EventHandler(this.清空文本框ToolStripMenuItem_Click);
+            // 
+            // openHex
+            // 
+            this.openHex.DefaultExt = "hex";
+            this.openHex.Filter = "hex文件|*.hex";
+            this.openHex.Multiselect = true;
+            this.openHex.Title = "打开hex文件";
+            // 
+            // saveBin
+            // 
+            this.saveBin.DefaultExt = "bin";
+            this.saveBin.Filter = "bin文件|*.bin";
+            this.saveBin.Title = "另存为";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(799, 450);
+            this.Controls.Add(this.txbLog);
             this.Controls.Add(this.dataGridView);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -130,17 +295,37 @@ namespace CSharp_Hex2Bin
             this.Opacity = 0.9D;
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.dataViewMenu.ResumeLayout(false);
+            this.cmbLogMenu.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.TextBox txbLog;
+        private System.Windows.Forms.ContextMenuStrip dataViewMenu;
+        private System.Windows.Forms.ContextMenuStrip cmbLogMenu;
+        private System.Windows.Forms.ToolStripMenuItem 清空文本框ToolStripMenuItem;
         private System.Windows.Forms.DataGridViewTextBoxColumn FliePath;
         private System.Windows.Forms.DataGridViewTextBoxColumn FileSize;
         private System.Windows.Forms.DataGridViewTextBoxColumn StartAddr;
         private System.Windows.Forms.DataGridViewTextBoxColumn EndAddr;
+        private System.Windows.Forms.ToolStripMenuItem 打开文件ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 生成binToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bin另存为ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 删除ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 打开记录ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem5;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem6;
+        private System.Windows.Forms.ToolStripMenuItem 填充空段ToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openHex;
+        private System.Windows.Forms.SaveFileDialog saveBin;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -17,26 +17,96 @@ namespace CSharp_Hex2Bin
         public Form1()
         {
             InitializeComponent();
+            SaveFilePath("");
         }
 
+        /// <summary>
+        /// 保存加载到内存的hex文件信息
+        /// </summary>
         List<Section> sections = new List<Section>();
+
         private void dataGridView_DragDrop(object sender, DragEventArgs e)
         {
             e.Effect = DragDropEffects.None;
-            string[] str = (string[])e.Data.GetData(DataFormats.FileDrop);
-            MyHex2Bin.GetAddrInfo(Encoding.Default, ref sections, out string log, str);
 
+            string[] str = (string[])e.Data.GetData(DataFormats.FileDrop);
+            OpenHexFile(str);
+        }
+        /// <summary>
+        /// 加hex文件信息加载到表格中
+        /// </summary>
+        /// <param name="sections">hex文件信息</param>
+        /// <returns>日志信息</returns>
+        string DataGridViewRefresh(List<Section> sections)
+        {
+            List<Section> errSections = new List<Section>();
+            MyHex2Bin.CheckAddr(sections, out string log, ref errSections);
+            dataGridView.Rows.Clear();
             foreach (var item in sections)
             {
                 DataGridViewRow dataGridViewRow = new DataGridViewRow();
                 dataGridViewRow.CreateCells(dataGridView);
                 dataGridViewRow.Cells[0].Value = item.filePath;
                 dataGridViewRow.Cells[1].Value = item.data.Count().ToString();
-                dataGridViewRow.Cells[2].Value = item.startAddr.ToString("X");
-                dataGridViewRow.Cells[3].Value = item.endAddr.ToString("X");
+                dataGridViewRow.Cells[2].Value = "0x" + item.startAddr.ToString("X8");
+                dataGridViewRow.Cells[3].Value = "0x" + item.endAddr.ToString("X8");
+                if (errSections.IndexOf(item) != -1)
+                {
+                    dataGridViewRow.DefaultCellStyle.BackColor = Color.DarkRed;
+                }
                 dataGridView.Rows.Add(dataGridViewRow);
             }
+            return log;
         }
+        /// <summary>
+        /// 记录历史文件信息
+        /// </summary>
+        /// <param name="path"></param>
+        void SaveFilePath(params string[] path)
+        {
+            List<string> strList = new List<string>();
+
+            strList.Add(Properties.Settings.Default.logPath1);
+            strList.Add(Properties.Settings.Default.logPath2);
+            strList.Add(Properties.Settings.Default.logPath3);
+            strList.Add(Properties.Settings.Default.logPath4);
+            strList.Add(Properties.Settings.Default.logPath5);
+            foreach (var item in path)
+            {
+                if (!string.IsNullOrWhiteSpace(item))
+                {
+                    strList.Remove(item);
+                    strList.Insert(0, item);
+                }
+            }
+
+            ///* 保存当前路径 */
+            Properties.Settings.Default.logPath1 = strList[0];
+            Properties.Settings.Default.logPath2 = strList[1];
+            Properties.Settings.Default.logPath3 = strList[2];
+            Properties.Settings.Default.logPath4 = strList[3];
+            Properties.Settings.Default.logPath5 = strList[4];
+            Properties.Settings.Default.Save();
+
+            /* 历史文件菜单 */
+            toolStripMenuItem2.Text = strList[0];
+            toolStripMenuItem3.Text = strList[1];
+            toolStripMenuItem4.Text = strList[2];
+            toolStripMenuItem5.Text = strList[3];
+            toolStripMenuItem6.Text = strList[4];
+        }
+        /// <summary>
+        /// 打开hex文件
+        /// </summary>
+        /// <param name="path"></param>
+        void OpenHexFile(params string[] path)
+        {
+            MyHex2Bin.LoadHex(Encoding.Default, ref sections, out string log, path);
+            log += DataGridViewRefresh(sections);
+            txbLog.AppendText(log);
+            SaveFilePath(path);
+        }
+        
 
         private void dataGridView_DragEnter(object sender, DragEventArgs e)
         {
@@ -55,5 +125,71 @@ namespace CSharp_Hex2Bin
             }
         }
 
+        private void 删除ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow item in dataGridView.SelectedRows)
+            {
+                sections.RemoveAt(item.Index);
+                dataGridView.Rows.RemoveAt(item.Index);
+            }
+        }
+
+        private void 生成binToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (sections.Count > 0)
+            {
+                if (填充空段ToolStripMenuItem.Checked)
+                {
+                    MyHex2Bin.WriteBinFile(sections[0].filePath, 0, sections);
+                }
+                else
+                {
+                    MyHex2Bin.WriteBinFile(sections[0].filePath, 0xff, sections);
+                }
+            }
+        }
+        
+        private void bin另存为ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (sections.Count > 0)
+            {
+                saveBin.InitialDirectory = Path.GetDirectoryName(sections[0].filePath);
+                saveBin.FileName = Path.GetFileNameWithoutExtension(sections[0].filePath);
+                if (saveBin.ShowDialog() == DialogResult.OK)
+                {
+                    if (填充空段ToolStripMenuItem.Checked)
+                    {
+                        MyHex2Bin.WriteBinFile(saveBin.FileName, 0, sections);
+                    }
+                    else
+                    {
+                        MyHex2Bin.WriteBinFile(saveBin.FileName, 0xff, sections);
+                    }
+                }
+            }
+        }
+
+        private void 打开文件ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openHex.ShowDialog() == DialogResult.OK)
+            {
+                OpenHexFile(openHex.FileNames);
+            }
+        }
+
+        private void 填充空段ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            填充空段ToolStripMenuItem.Checked = !填充空段ToolStripMenuItem.Checked;
+        }
+
+        private void 清空文本框ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            txbLog.Text = "";
+        }
+
+        private void toolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenHexFile(((ToolStripMenuItem)sender).Text);
+        }
     }
 }
